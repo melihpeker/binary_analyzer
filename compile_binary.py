ccb376a42f35c7eeb684aa921f518ad680882d7d
melihpeker <31694777+melihpeker@users.noreply.github.com>
2024-07-28 14:56:19 +0300

    Update compile_binary.py

--- compile_binary.py
+++ compile_binary.py
@@ -28,10 +28,10 @@ def compile_binary_generic(path, name, options, index):
 
 
 def compile_binary(path, name, output_path, options):
-    command = "gcc-8 -O2 " + options + " -I dataset/polybench-c-3.2/utilities -I " + \
+    command = "gcc -O2 " + options + " -I dataset/polybench-c-3.2/utilities -I " + \
               path + " dataset/polybench-c-3.2/utilities/polybench.c " +\
               name + " -DPOLYBENCH_TIME -o " + output_path
-    # command = "gcc-8 -I dataset/polybench-c-3.2/utilities -I dataset/polybench-c-3.2/linear-algebra/kernels/atax dataset/polybench-c-3.2/utilities/polybench.c dataset/polybench-c-3.2/linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o atax_base"
+    # command = "gcc -I dataset/polybench-c-3.2/utilities -I dataset/polybench-c-3.2/linear-algebra/kernels/atax dataset/polybench-c-3.2/utilities/polybench.c dataset/polybench-c-3.2/linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o atax_base"
     # os.system(command)
     # command = "gcc -v"
     env = os.environ.copy()
